60c036b0b48544c3b7a1ec6601c4f89a70961fe4
Castle <castlez93@gmail.com>
2017-07-20 20:27:15 -0700

    adding more stuff

--- mac_setup.sh
+++ mac_setup.sh
@@ -14,3 +14,26 @@ echo 'export PATH=/usr/local/homebrew/bin:$PATH' >> ~/.zshrc
 
 # fancy git log alias 'lg'
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+# awesome vimrc from GitHub user amix
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_basic_vimrc.sh
+
+# pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+exec $SHELL
+
+# python
+pyenv install 3.6.1
+pyenv global 3.6.1
+
+# redis
+brew install redis
+
+# virtualenv
+pip install virtualenv
+
+
