2220d00792ba17fd11196211e154c64611d9cc9b
Castle <castlez93@gmail.com>
2024-05-16 22:33:18 -0700

    need pyenv deps

--- apt_linux_setup.sh
+++ apt_linux_setup.sh
@@ -29,6 +29,9 @@ git config --global user.name "Castle"
 git config --global user.email "castlez93@gmail.com"
 
 echo "setup pyenv"
+sudo apt install build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
