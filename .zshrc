922e26b6991ff76f2574b5809b62769ea7a6df35
Castle <jcastle@halcyon.ai>
2026-02-10 21:04:11 -0800

    update shit

--- .zshrc
+++ .zshrc
@@ -1,46 +1,60 @@
 # If you come from bash you might have to change your $PATH.
-export PATH=$HOME/bin:/usr/local/bin:$PATH
-
-# Path to your oh-my-zsh installation.
-export ZSH="$HOME/.oh-my-zsh"
-
-# hostname set for cli
-HOSTNAME=$HOST
-
-# Set name of the theme to load. Optionally, if you set this to "random"
-# # it'll load a random theme each time that oh-my-zsh is loaded.
-# # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="robbyrussell"
-SPACESHIP_VI_MODE_SHOW=false
-SPACESHIP_PROMPT_ADD_NEWLINE=false
-SPACESHIP_HOST_SHOW=true
-SPACESHIP_HOST_SHOW_FULL=true
-SPACESHIP_VENV_SYMBOL="🐍 "
-SPACESHIP_PROMPT_ORDER=(host venv git dir exec_time line_sep char)
-
-# Which plugins would you like to load?
-# Standard plugins can be found in ~/.oh-my-zsh/plugins/*
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
-
-# User configuration
-
-source $ZSH/oh-my-zsh.sh
-
-# aliases
-
-alias gmp="git checkout master;git pull"
-
-# ls after cd
-function chpwd() {
-    emulate -L zsh
-    ls -a
-}
-
-export PROMPT='---------------
-%(!.%F{yellow}.)$USER%F{white}@%M ${ret_status} %F{cyan}%c%f $(git_prompt_info)
-%F{green}➜  %F{white}'
-
-
+ export PATH=$HOME/bin:/usr/local/bin:$PATH
+ 
+ # hostname set for cli
+ HOSTNAME=$HOST
+ # Set name of the theme to load. Optionally, if you set this to "random"
+ # # it'll load a random theme each time that oh-my-zsh is loaded.
+ # # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
+ ZSH_THEME="robbyrussell"
+ SPACESHIP_VI_MODE_SHOW=false
+ SPACESHIP_PROMPT_ADD_NEWLINE=false
+ SPACESHIP_HOST_SHOW=true
+ SPACESHIP_HOST_SHOW_FULL=true
+ SPACESHIP_VENV_SYMBOL="🐍 "
+ SPACESHIP_PROMPT_ORDER=(host venv git dir exec_time line_sep char)
+ 
+ plugins=(git)
+ 
+ source $HOME/.oh-my-zsh/oh-my-zsh.sh
+ 
+ # User configuration
+ 
+ function chpwd(){
+     emulate -L zsh
+     ls -a
+ }
+ 
+ export TERM=xterm-256color
+ 
+ export PYENV_ROOT="$HOME/.pyenv"
+ [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+ eval "$(pyenv init - bash)"
+ 
+ eval "$(pyenv virtualenv-init -)"
+ 
+ 
+ # Auto-update Python packages on SSH login
+ if [[ -n "$SSH_CONNECTION" ]]; then
+     {
+         # Check if updates are available without installing
+         outdated=$(python3 -m pip list --outdated --format=freeze 2>/dev/null | grep -E '^(agent_core|cloud_support)=')
+ 
+         # Only update if packages are outdated
+         if [[ -n "$outdated" ]]; then
+             python3 -m pip install --upgrade agent_core cloud_support
+         fi
+     } &>/dev/null || {
+         echo "Warning: Failed to check/update Python packages (agent_core, cloud_support)"
+     }
+ fi
+ 
+ export PIP_NO_CACHE_DIR=1
+ 
+ export PROMPT='---------------
+ %(!.%F{yellow}.)$USER%F{white}@%M ${ret_status} %F{cyan}%c%f $(git_prompt_info)
+ %F{green}➜  %F{white}'
+ 
+ [[ -s /home/qauser/.autojump/etc/profile.d/autojump.sh ]] && source /home/qauser/.autojump/etc/profile.d/autojump.sh
+ autoload -U compinit && compinit -u
+ 
---
+++ move_in_linux.sh
@@ -0,0 +1,163 @@
+mkdir -p ~/src
+# clone notes and copy over dot files
+sudo dnf install autojump -y
+sudo dnf install git -y
+sudo dnf install vim -y
+sudo dnf install ctags -y
+
+
+# my gitconfig
+echo """
+[user]
+        email = jcastle@halcyon.ai
+        name = Castle
+[push]
+        autoSetupRemote = true
+[pull]
+        rebase = true
+[core]
+        hooksPath = /home/qauser/.git-hooks
+[credential "https://github.com"]
+        helper = 
+        helper = !/usr/bin/gh auth git-credential
+[credential "https://gist.github.com"]
+        helper = 
+        helper = !/usr/bin/gh auth git-credential
+[alias]
+        amend = commit --amend --no-edit
+        lg = log --graph --oneline --all --decorate
+        crush = !git fetch && git reset --hard @{u}
+        st = status
+        up = !git add . && git commit --amend --no-edit && git push -f
+""" > ~/.gitconfig
+echo "Wrote ~/.gitconfig"
+
+echo """
+Git Cheatsheet
+================
+check local branches - git branch
+check all branches - git branch -a
+check remote branches - git branch -r
+
+TMUX
+================
+
+Leader: Ctrl-b
+
+### Navigation
+Leader + o           : next pane (cycle through panes)
+Leader + ;           : previous pane (toggle between current and previous)
+Leader + arrow keys  : select pane by direction
+Leader + n           : next window/tab
+Leader + p           : previous window/tab
+Leader + 0-9         : select window by number
+Leader + q           : show pane numbers (then press number to switch)
+
+### With mouse enabled (set -g mouse on)
+Click pane           : select pane
+Click window name    : switch window
+Drag pane border     : resize pane
+
+### With Option+arrow bindings (bind -n M-Left select-pane -L)
+Option + arrow keys  : select pane by direction (no prefix needed)
+
+### Splitting Panes
+Leader + \"           : split horizontally (one above the other)
+Leader + %           : split vertically (side by side)
+
+### Closing/Exiting
+Ctrl-d               : close current pane (or type 'exit')
+Leader + x           : kill current pane (prompts for confirmation)
+Leader + &           : kill current window (prompts for confirmation)
+Leader + d           : detach from session (keeps it running)
+
+### Session Management
+tmux ls              : list sessions
+tmux attach -t name  : reattach to session
+tmux kill-session -t name : kill entire session
+
+### Other Useful Operations
+Leader + z           : zoom/unzoom pane (fullscreen toggle)
+Leader + ,           : rename window
+Leader + :           : command prompt
+Leader + ?           : list all keybindings
+Leader + Ctrl-o      : swap panes
+Ctrl-b :source-file ~/.tmux.conf : reload config
+
+Vim (some of these require my vimrc)
+================
+CTAGS
+----------------
+To generate ctags for a Python project, run the following command in the root directory of your project:
+
+    ctags -R --languages=python --python-kinds=-i --exclude=.git --exclude='*.pyc' --exclude=__pycache__ . ./venv/lib/python*/site-packages
+
+In Vim
+- ctrl + ] : Jump to the definition of the word under the cursor
+- ctrl + o : Jump back from the definition in current tab
+- ctrl + t : Jump back from the definition in previous tab and close the current tab
+- \ + u : find usages
+
+Navigation
+----------------
+- h, j, k, l : Move left, down, up, right
+- gg : Go to the beginning of the file
+- G : Go to the end of the file
+- :n : Go to line number n
+- w : Move to the beginning of the next word
+- b : Move to the beginning of the previous word
+- Ctrl + f : Move forward one page
+- Ctrl + b : Move backward one page
+- click on the fold column to the left of the line numbers to fold/unfold code blocks (requires ctags?)
+-- NOTE: you have to click the "-" or "+" symbol to fold/unfold, clicking the line number itself won't work
+- zM : Fold all code blocks
+- zR : Unfold all code blocks
+- zo : Open fold at the cursor
+- zc : Close fold at the cursor
+- \ + f: alias for zM (in vimrc)
+- ":help fold-commands" to learn more about folding
+
+Editing
+-------
+- i : Insert mode before the cursor
+- a : Insert mode after the cursor
+- o : Open a new line below the current line and enter insert mode
+- O : Open a new line above the current line and enter insert mode
+- r : Replace a single character
+- x : Delete the character under the cursor
+- dd : Delete the current line
+- yy : Yank (copy) the current line
+- p : Paste after the cursor
+- P : Paste before the cursor
+- u : Undo the last action
+- Ctrl + r : Redo the last undone action
+
+Searching
+---------
+- /pattern : Search for 'pattern' forward
+- ?pattern : Search for 'pattern' backward
+- n : Repeat the last search in the same direction
+- N : Repeat the last search in the opposite direction
+
+Saving and Exiting
+-------------------
+- :w : Save the file
+- :q : Quit Vim
+- :wq or :x : Save and quit
+- :q! : Quit without saving
+
+Visual Mode
+-----------
+- v : Enter visual mode (character-wise selection)
+- V : Enter visual line mode (line-wise selection)
+- Ctrl + v : Enter visual block mode (block-wise selection)
+- y : Yank (copy) the selected text
+- d : Delete the selected text
+- > : Indent the selected text
+- < : Unindent the selected text
+
+Help
+----
+- :help : Open Vim help documentation
+""" > ~/cheatsheet.txt
+cat ~/cheatsheet.txt
